2f24040d395c89450afc8b7bf0e062682303af5f
nirab123456 <rifatulislam210@gmail.com>
2025-10-08 13:30:17 -0400

    PREBUILD::PREBUILD::default macros, shift to enter,ctrl,fn nt working

--- src/oled_debug.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <Arduino.h>
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-#include <freertos/FreeRTOS.h>
-#include <freertos/queue.h>
-bool oled_INIT(uint8_t i2c_addr = 0x3C, int width = 128, int height = 64, int sda_pin = 8, int scl_pin = 9);
-void oled_LOGF(const char* fmt,...);
